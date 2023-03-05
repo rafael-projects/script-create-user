d92fea5d135a6ae35e6782ae598a539fdcd0c807
rafael-projects <rss4110@hotmail.com>
2023-03-05 19:29:16 -0300

    script

---
+++ create_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Criando usuários do sistema...."
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m 
+passwd guest10 -e
+
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m 
+passwd guest11 -e
+
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m 
+passwd guest12 -e
+
+useradd guest13 -c "Usuário convidado" -s /bin/bash -m
+passwd guest13 -e
+
+echo "Finalizado!!"
